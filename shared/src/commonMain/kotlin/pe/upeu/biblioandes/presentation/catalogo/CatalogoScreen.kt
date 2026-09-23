7731a62d7643b04872b65d11384b566923d5788c
Jeanpierre Miranda <jesamiga11@hotmail.com>
2026-09-23 16:01:19 -0500

    feat(catalogo): mostrar la editorial bajo el autor

--- shared/src/commonMain/kotlin/pe/upeu/biblioandes/presentation/catalogo/CatalogoScreen.kt
+++ shared/src/commonMain/kotlin/pe/upeu/biblioandes/presentation/catalogo/CatalogoScreen.kt
@@ -96,6 +96,7 @@ fun CatalogoScreen(
                         LibroItem(
                             titulo = libro.titulo,
                             autor = libro.autor,
+                            editorial = libro.editorial,
                             ejemplares = libro.ejemplaresDisponibles,
                             disponible = ReglasPrestamo.tieneEjemplares(libro),
                             onClick = { onAbrirLibro(libro.id) }
